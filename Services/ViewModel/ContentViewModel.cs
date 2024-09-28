166d5db0744d629a1a54ede627801f13480f0ea0
Muhammed Maklad <mohammedmaklad97@gmail.com>
2024-09-28 10:43:36 +0300

    Form Validation

--- Services/ViewModel/ContentViewModel.cs
+++ Services/ViewModel/ContentViewModel.cs
@@ -9,9 +9,12 @@ namespace TechWebApplication.Services.ViewModel
         [Required(ErrorMessage ="Please Provide Title for Content")]
         [Length(5,80, ErrorMessage ="Provide Valid Title with at least 5 character")]
         public string Title { get; set; }
+        
+        [Display(Name = "HTML Content")]
         public string? HTMLContent { get; set; }
 
         [RegularExpression(@"^https?:\/\/([a-zA-Z0-9\-_]+\.)+[a-zA-Z]{2,}(:[0-9]{1,5})?(\/[a-zA-Z0-9\-._~:\/?#\[\]@!$&'()*+,;=%]*)?$", ErrorMessage = "Not a valid URL")]
+        [Display(Name = "Video Link")]
         public string? VideoLink { get; set; }
 
         public int CategoryItemId { get; set; }
