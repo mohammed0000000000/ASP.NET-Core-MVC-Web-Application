50282e9dbce94da2fb5d07f21d224dfb009e18e4
Muhammed Maklad <mohammedmaklad97@gmail.com>
2024-10-11 21:25:09 +0300

    all function done

--- wwwroot/js/RegisterCard.js
+++ wwwroot/js/RegisterCard.js
@@ -1,17 +1,19 @@
 ﻿$(function () {
     let registerModel = $("#UserRegistrationModal");
     console.log("This is register mOdle ==> " + registerModel);
-    $(".card-click").on("click", function() {
-        var categoryId = $(this).find("input").val();
+    $(".card-click").on("click", function () {
+        var categoryId = $(this).data("id");
+        console.log("this is id for card => " + categoryId)
         $.ajax({
             async: true,
             type: "POST",
             url: "https://localhost:44309/UsersToCategory/AssignCourseToUser?categoryId=" + categoryId,
             success: function (response, status, jqxhr) {
+                console.log(response);
                 if (response == 'Unauhorized') {
                     registerModel.modal("show");
                 }
-                else if (rseponse == 'false') {
+                else if (response == 'false') {
                     
                 }
                 else {
